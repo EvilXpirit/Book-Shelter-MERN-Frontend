00fb6b995b08623130d6127dfa3066b0c1c1e0d0
ADitya SHarma <adsharma122@gmail.com>
2024-07-09 18:13:15 +0530

    autoplay

--- src/components/NewArrivalsSwiper.jsx
+++ src/components/NewArrivalsSwiper.jsx
@@ -4,10 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import "swiper/css/autoplay";
+import { Autoplay } from "swiper/modules";
 import { Pagination } from 'swiper/modules';
 import baseUrl from '../../Urls';
 
-const NewArrivalsSwiper = ({ book, openModal}) => {
+const NewArrivalsSwiper = ({ book, openModal }) => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
@@ -23,44 +25,43 @@ const NewArrivalsSwiper = ({ book, openModal}) => {
 
     fetchBooks();
   }, []);
-  
 
   return (
     <div className="w-full p-4 overflow-visible">
       <Swiper
-      className=''
-        modules={[Pagination]}
+        className=""
+        modules={[Pagination, Autoplay]}
         pagination={{ clickable: true }}
         spaceBetween={20}
         slidesPerView={1}
         breakpoints={{
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 4,
-            spaceBetween: 50,
-          },
+          640: { slidesPerView: 2, spaceBetween: 20 },
+          768: { slidesPerView: 3, spaceBetween: 40 },
+          1024: { slidesPerView: 4, spaceBetween: 50 },
         }}
+        autoplay={{ delay: 3000, pauseOnMouseEnter: true }}
       >
         {books.map((book) => (
           <SwiperSlide key={book._id}>
             <div className="bg-white/75 rounded-lg shadow-lg h-96 overflow-hidden mb-10 transition-all duration-200 ease-in-out hover:bg-white/90 transform hover:drop-shadow-2xl">
-            <div className=''>
-              <img src={book.imageUrl} alt={book.bookName} className=" w-full h-48 object-cover object-top transition-all duration-200 ease-in-out transform hover:h-full hover:scale-105" />
-
-            </div>
+              <div className="">
+                <img
+                  src={book.imageUrl}
+                  alt={book.bookName}
+                  className="w-full h-48 object-cover object-top transition-all duration-200 ease-in-out transform hover:h-full hover:scale-105"
+                />
+              </div>
               <div className="flex-grow mt-4 ms-4 space-y-2 mb-4">
                 <h3 className="text-lg font-bold">{book.bookName}</h3>
                 <p className="text-gray-600">{book.authorName}</p>
                 <p className="text-gray-800">₹ {book.price}</p>
               </div>
-              <button onClick={() => openModal(book)} className="mx-2 px-2 py-2 bg-yellow-500 text-white rounded hover:shadow-lg"><FontAwesomeIcon icon={faEye} size="lg" /></button>
+              <button
+                onClick={() => openModal(book)}
+                className="mx-2 px-2 py-2 bg-yellow-500 text-white rounded hover:shadow-lg"
+              >
+                <FontAwesomeIcon icon={faEye} size="lg" />
+              </button>
             </div>
           </SwiperSlide>
         ))}
